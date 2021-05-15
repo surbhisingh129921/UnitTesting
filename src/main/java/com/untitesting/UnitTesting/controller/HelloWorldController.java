78e3a55c15bf544f35e1638543f35829f1e7b923
Saurabh <emosrb@gmail.com>
2021-05-15 12:42:33 +0530

    Created HelloWorldController Mock Test Class

--- src/main/java/com/untitesting/UnitTesting/controller/HelloWorldController.java
+++ src/main/java/com/untitesting/UnitTesting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.untitesting.UnitTesting;
+package com.untitesting.UnitTesting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/test/java/com/untitesting/UnitTesting/controller/HelloWorldControllerTest.java
@@ -0,0 +1,28 @@
+package com.untitesting.UnitTesting.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMVC;
+
+    @Test
+    public void hello_worldBasic() throws Exception{
+        RequestBuilder request = MockMvcRequestBuilders.get("/hello-world")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMVC.perform(request).andReturn();
+
+        assertEquals("Hello World",result.getResponse().getContentAsString());
+    }
+}
